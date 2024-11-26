1e99487a9d9cbb94dbc496e5acb5a564ce182c38
Egor Solomonov <solomonov-egor@mail.ru>
2024-11-26 17:50:05 +0300

    Renamed functions to setdate_functions

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,4 @@
 import { today_date_obj } from "./utils/vars.js";
-import { setDayOfWeek, setDate } from "./utils/functions.js";
+import { setDayOfWeek, setDate } from "./utils/setdate_functions.js";
 setDayOfWeek(today_date_obj);
 setDate(today_date_obj);
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { day_of_week_DOM, date_DOM } from "./utils/dom_vars.js";
 import { int_to_day_of_week_string } from "./utils/dicts.js";
 import { today_date_obj } from "./utils/vars.js";
-import { setDayOfWeek, setDate } from "./utils/functions.js";
+import { setDayOfWeek, setDate } from "./utils/setdate_functions.js";
 
 
 setDayOfWeek(today_date_obj)
