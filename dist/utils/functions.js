14f9b2a600f4bc06636166f6e4f115d1157503c6
Egor Solomonov <solomonov-egor@mail.ru>
2024-11-26 16:24:39 +0300

    Unknown changes

--- dist/utils/functions.js
+++ dist/utils/functions.js
@@ -1,7 +1,7 @@
-import { int_to_dayOfWeek } from "./dicts.js";
+import { int_to_day_of_week } from "./dicts.js";
 import { day_of_week_DOM } from "./dom_vars.js";
 function setDayOfWeek(int_weekday) {
-    let todays_day_string = int_to_dayOfWeek[int_weekday];
+    let todays_day_string = int_to_day_of_week[int_weekday];
     day_of_week_DOM.textContent = todays_day_string;
 }
 export { setDayOfWeek };
