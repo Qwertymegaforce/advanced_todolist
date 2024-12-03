9c24290bcd7025356a7593f4fd7e3e052fd66f84
Egor Solomonov <solomonov-egor@mail.ru>
2024-12-03 17:38:36 +0300

    Added icon for timeselection button

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -1,3 +1,5 @@
 export const check_circle_url = '../../assets/nav/check_circle.svg'
 
 export const cancel_circle_url = '../../assets/nav/cancel_circle.svg'
+
+export const time_selection_url = '../../assets/nav/time_selection.svg'
