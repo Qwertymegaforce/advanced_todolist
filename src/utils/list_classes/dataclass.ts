026e9d4a64ed28a359dc42deb0e78afebdb929fa
Egor Solomonov <solomonov-egor@mail.ru>
2024-12-04 16:52:16 +0300

    Added tracker for state of form as well as the method which provides the functionality to change this tracker

--- src/utils/list_classes/dataclass.ts
+++ src/utils/list_classes/dataclass.ts
@@ -1,6 +1,10 @@
 class DataStorage {
     
     public timeselection_form_is_displayed = false;
+
+    public changeStateOfFormDisplayed(): void {
+        this.timeselection_form_is_displayed = !this.timeselection_form_is_displayed
+    }
 }
 
 
