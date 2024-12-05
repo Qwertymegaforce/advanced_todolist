efb996620c0aac84fa8d5c158920460f342a423e
Egor Solomonov <solomonov-egor@mail.ru>
2024-12-05 16:50:09 +0300

    Added check mark svg

--- src/utils/urls.ts
+++ src/utils/urls.ts
@@ -3,3 +3,5 @@ export const check_circle_url = '../../assets/nav/check_circle.svg'
 export const cancel_circle_url = '../../assets/nav/cancel_circle.svg'
 
 export const time_selection_url = '../../assets/nav/time_selection.svg'
+
+export const check_url = '../../assets/others/check.svg'
