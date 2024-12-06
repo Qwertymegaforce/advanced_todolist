d2bc40c985ea1d4f0d349fb8470012138beec09f
Egor Solomonov <solomonov-egor@mail.ru>
2024-12-06 17:51:04 +0300

    Created Id counter in dataclass

--- src/utils/list_classes/dataclass.ts
+++ src/utils/list_classes/dataclass.ts
@@ -1,9 +1,14 @@
 class DataStorage {
     
     public timeselection_form_is_displayed = false;
+    public actualUniqueToDoId = 0;
 
     public changeStateOfFormDisplayed(): void {
         this.timeselection_form_is_displayed = !this.timeselection_form_is_displayed
+    }
+
+    public updateActualUniqueTodoId(): void {
+        this.actualUniqueToDoId += 1
     }
 }
 
