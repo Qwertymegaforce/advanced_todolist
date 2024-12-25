b26f4a3cd3837a41abeb2720f8e02767dbed6ed4
Egor Solomonov <solomonov-egor@mail.ru>
2024-12-25 16:53:11 +0300

    Moved event listner and added import

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,9 @@
 import { today_date_obj } from "./utils/vars.js";
 import { setDayOfWeek, setDate } from "./utils/setdate_functions.js";
-import { addbutton_DOM } from "./utils/dom_vars.js";
-import { displayToDoCreationForm, updateTaskCounter } from "./utils/list_functions.js";
+import "./utils/dom_vars.js";
+import {updateTaskCounter } from "./utils/list_functions.js";
+import { updatePageTodoList } from "./utils/list_functions.js";
 
 setDayOfWeek(today_date_obj)
 setDate(today_date_obj)
 updateTaskCounter()
-
-addbutton_DOM.addEventListener("click", () => {
-    displayToDoCreationForm()
-    addbutton_DOM.style.pointerEvents = "none"
-})
-
-
-
-
-
