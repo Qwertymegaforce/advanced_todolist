8d6808716a6ac7e6847ddaf4998b2fbf1e3ebcbc
Egor Solomonov <solomonov-egor@mail.ru>
2024-12-25 16:52:51 +0300

    Moved event listener

--- src/utils/dom_vars.ts
+++ src/utils/dom_vars.ts
@@ -1,3 +1,5 @@
+import { displayToDoCreationForm } from "./list_functions.js";
+
 let day_of_week_DOM = document.getElementById('weekday') as HTMLSpanElement;
 
 let date_DOM = document.getElementById('date') as HTMLSpanElement;
@@ -7,6 +9,13 @@ let addbutton_DOM = document.getElementById('addtask') as HTMLButtonElement;
 let task_list_DOM = document.getElementById('task_list') as HTMLDivElement;
 
 let task_counter_DOM = document.getElementById('taskcounter') as HTMLSpanElement
+
+
+addbutton_DOM.addEventListener("click", () => {
+    displayToDoCreationForm()
+    addbutton_DOM.style.pointerEvents = "none"
+})
+
 
 export {
     day_of_week_DOM, 
