9ff67bdbaa128c3181e1b011153910676771bc56
Egor Solomonov <solomonov-egor@mail.ru>
2024-12-25 17:32:17 +0300

    Used new function to update the whole page

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { today_date_obj } from "./utils/vars.js";
-import { setDayOfWeek, setDate } from "./utils/setdate_functions.js";
 import "./utils/dom_vars.js";
-import {updateTaskCounter } from "./utils/list_functions.js";
-import { updatePageTodoList } from "./utils/list_functions.js";
+import { updateWholePage } from "./utils/list_classes/util_functions.js";
 
-setDayOfWeek(today_date_obj)
-setDate(today_date_obj)
-updateTaskCounter()
+
+updateWholePage(today_date_obj)
