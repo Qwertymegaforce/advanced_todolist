ffd3c1f464e43f2dabdb7a74d0f1056ffa63e790
Egor Solomonov <solomonov-egor@mail.ru>
2024-12-27 15:58:51 +0300

    Called new func

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { today_date_obj } from "./utils/vars.js";
 import "./utils/dom_vars.js";
-import { updateWholePage } from "./utils/list_classes/util_functions.js";
+import { updateWholePage, updateIdCounterOfTodos } from "./utils/list_classes/util_functions.js";
 
 
+updateIdCounterOfTodos()
 updateWholePage(today_date_obj)
