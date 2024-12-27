88e21f7ee8f07bbe3366ccdf13d87f5367376d28
Egor Solomonov <solomonov-egor@mail.ru>
2024-12-27 15:57:14 +0300

    Cleared useleess piaces of code

--- src/utils/vars.ts
+++ src/utils/vars.ts
@@ -1,6 +1,5 @@
 import type { toDo_task_type } from "../types/todo_types"
 import { getLSKeyFromDateObj } from "./etc/sys_func.js"
-import { arrayConsistOfTodos } from "./list_classes/util_functions.js"
 
 
 let today_date_obj = new Date()
@@ -8,7 +7,6 @@ let todo_list: Array<toDo_task_type> = []
 
 let ls_key = getLSKeyFromDateObj(today_date_obj)
 let ls_data = localStorage.getItem(ls_key)
-let ls_decoded_data
 
 if(typeof ls_data == "string") {
     todo_list = JSON.parse(ls_data)
