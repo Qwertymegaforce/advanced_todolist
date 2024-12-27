a34666fa07f545ceb5e4db12f5a3a3b9c4d5b686
Egor Solomonov <solomonov-egor@mail.ru>
2024-12-27 16:05:32 +0300

    Called redefined func

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { today_date_obj } from "./utils/vars.js";
 import "./utils/dom_vars.js";
-import { updateWholePage, updateIdCounterOfTodos } from "./utils/list_classes/util_functions.js";
+import { updateWholePage} from "./utils/list_classes/util_functions.js";
+import { data_storage } from "./utils/list_classes/dataclass.js";
 
 
-updateIdCounterOfTodos()
+data_storage.loadUniqueIdFromLS(today_date_obj)
 updateWholePage(today_date_obj)
